c76cff54cd2b4e519f1b6f9d2318b04f6458419c
Alex Ficklin and Nicko Dela Cruzß <alex@>
2018-01-11 15:00:40 -0800

    Add HTML, CSS and JS to create triangle website.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var getType = function(digit1, digit2, digit3) {
+  if (digit1===digit2 && digit2===digit3) {
+    $('#equal').show();
+
+    // return "#equal";
+  } else if (digit1===digit2 && digit2!=digit3) {
+      $('#iso').show();
+
+      // return "#iso";
+  } else if (digit1!=digit2 && digit2!=digit3) {
+    $('#scale').show();
+  } else 
+    $('#none').show();
+  };
+
+
+
+
+$(document).ready(function() {
+  $("form#triangleTracker").submit(function(event) {
+    event.preventDefault();
+    var digit1 = parseInt($("#digit1").val());
+    var digit2 = parseInt($("#digit2").val());
+    var digit3 = parseInt($("#digit3").val());
+
+    getType(digit1, digit2, digit3);
+
+  });
+
+});
